6aae17575c728ecdc57d847f31a5677fb0c5c413
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2022-03-26 16:31:57 +0900

    とりあえず数える

--- build.sbt
+++ build.sbt
@@ -2,12 +2,11 @@ ThisBuild / scalaVersion := "2.13.5"
 ThisBuild / version := "0.1.0-SNAPSHOT"
 ThisBuild / organization := "com.github.stoneream"
 
-lazy val root = (project in file("."))
-  .settings(
-    name := "scala-template",
-    libraryDependencies ++= Seq(
-      "org.scalatest" %% "scalatest" % "3.2.8"
-    )
+lazy val root = (project in file(".")).settings(
+  name := "dis-assembly",
+  libraryDependencies ++= Seq(
+    "org.scalatest" %% "scalatest" % "3.2.8"
   )
+)
 
 scalafmtOnCompile := true
--- src/main/scala/app/Main.scala
+++ src/main/scala/app/Main.scala
@@ -1,3 +1,25 @@
 package app
 
-object Main extends App {}
+import java.awt.Color
+import java.nio.file.Paths
+import javax.imageio.ImageIO
+
+object Main extends App {
+
+  val filePath = Paths.get("./image.jpg")
+  val file = filePath.toFile
+  val image = ImageIO.read(file)
+
+  val positions = (image.getMinY until image.getHeight).map { y =>
+    (image.getMinX until image.getWidth).map { x =>
+      (x, y)
+    }
+  }.flatten
+
+  val colorCodes = positions.map { case (x, y) => image.getRGB(x, y) }.groupBy(c => c).toList.sortBy { case (_, n) => n.size }
+
+  colorCodes.foreach { case (rgb, n) =>
+    val color = new Color(rgb)
+    println(s"#${color.getRed.toHexString}${color.getGreen.toHexString}${color.getBlue.toHexString}, ${n.size}")
+  }
+}
